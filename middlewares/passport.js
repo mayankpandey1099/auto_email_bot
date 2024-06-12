8d68de52d4b5c06639ccb15f9e445f8575a74530
mayank pandey <mayankpandey1099@gmail.com>
2024-06-12 17:20:41 +0530

    updated passport authentication

---
+++ middlewares/passport.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const tokenStore = require("../utils/tokenStore");
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      profile.tokens = { accessToken, refreshToken };
+      tokenStore.accessToken = accessToken;
+      tokenStore.refreshToken = refreshToken;
+      return done(null, profile);
+    }
+  )
+);
+
+module.exports = passport;
